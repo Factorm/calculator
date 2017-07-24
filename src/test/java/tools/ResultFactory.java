be6a18ae7151543da4816956bbacd74c4161cacc
factor.loster <factor.loster@gmail.com>
2017-07-24 10:54:24 +0200

    Update ResultFactory

--- src/test/java/tools/ResultFactory.java
+++ src/test/java/tools/ResultFactory.java
@@ -1,40 +1,41 @@
 package tools;
 
+import static java.lang.Math.*;
 public class ResultFactory {
 
 	public static boolean checkTheResult(String value1, String value2, String mathMethod, String pageResult) {
 
-		int localValue1 = Integer.parseInt(value1);
-		int localValue2 = Integer.parseInt(value2);
-		float localPageResult = Float.parseFloat(pageResult);
+		double localValue1 = Integer.parseInt(value1);
+		double localValue2 = Integer.parseInt(value2);
+		double localPageResult = Float.parseFloat(pageResult);
 
 		if (mathMethod.equals("ADD(+)")) {
 
-			int result = localValue1 + localValue2;
+			double result = localValue1 + localValue2;
 
 			return (localPageResult == result);
 
 		} else if (mathMethod.equals("SUBTRACT(-)")) {
 
-			int result = localValue1 - localValue2;
+			double result = localValue1 - localValue2;
 
 			return (localPageResult == result);
 
 		} else if (mathMethod.equals("MULTIPLY(*)")) {
 
-			int result = localValue1 * localValue2;
+			double result = localValue1 * localValue2;
 
 			return (localPageResult == result);
 
 		} else if (mathMethod.equals("DIVIDE(/)")) {
 
-			int result = localValue1 / localValue2;
+			double result = localValue1 / localValue2;
 
 			return (localPageResult == result);
 
 		} else if (mathMethod.equals("POWER(^)")) {
 
-			int result = localValue1 + localValue2;
+			double result = pow(localValue1, localValue2);
 
 			return (localPageResult == result);
 		}
